f314e10c6c764740f697e7124a855a080e4e9034
Sandor Bencze <Sandor_Bencze@epam.com>
2013-03-21 16:33:38 +0100

    Actualized table structures

--- elv-server/src/main/resources/tables.sql
+++ elv-server/src/main/resources/tables.sql
@@ -1,43 +1,34 @@
 ---- MORTALITY
 -- Create:
 CREATE TABLE mortality(
- death_year INT4 DEFAULT 0 NOT NULL,
- death_month INT2 DEFAULT 0 NOT NULL,
- death_day INT2 DEFAULT 0 NOT NULL,
- death_hour INT2 DEFAULT 0 NOT NULL,
- death_minute INT2 DEFAULT 0 NOT NULL,
- birth_year INT4 DEFAULT 0 NOT NULL,
- birth_month INT2 DEFAULT 0 NOT NULL,
- birth_day INT2 DEFAULT 0 NOT NULL,
- permanent_residence INT4 DEFAULT 0 NOT NULL,
- effective_residence INT4 DEFAULT 0 NOT NULL,
- gender INT2 DEFAULT 0 NOT NULL,
- age INT4 DEFAULT 0 NOT NULL,
- diagnosis_1 VARCHAR (5) DEFAULT '' NOT NULL,
- diagnosis_2 VARCHAR (5) DEFAULT '' NOT NULL,
- diagnosis_3 VARCHAR (5) DEFAULT '' NOT NULL,
- diagnosis_4 VARCHAR (5) DEFAULT '' NOT NULL,
- diagnosis_5 VARCHAR (5) DEFAULT '' NOT NULL,
- diagnoser INT2 DEFAULT 0 NOT NULL,
- medical_treatment INT2 DEFAULT 0 NOT NULL);
+ year INTEGER NOT NULL,
+ month SMALLINT DEFAULT 0 NOT NULL,
+ gender SMALLINT NOT NULL,
+ age INTEGER NOT NULL,
+ settlement INTEGER NOT NULL,
+ diagnosis_1 VARCHAR (5) DEFAULT '00000' NOT NULL,
+ diagnosis_2 VARCHAR (5) DEFAULT '00000' NOT NULL,
+ diagnosis_3 VARCHAR (5) DEFAULT '00000' NOT NULL,
+ diagnosis_4 VARCHAR (5) DEFAULT '00000' NOT NULL,
+ diagnosis_5 VARCHAR (5) DEFAULT '00000' NOT NULL);
 
 -- Index:
-CREATE INDEX mortality ON mortality(death_year, gender, age, effective_residence, death_month);
+CREATE INDEX mortality ON mortality(year, gender, age, settlement, month);
 
 -- Load:
-insert into mortality select * from csvread('F:/ELV/data/mortality.csv');
+INSERT INTO mortality SELECT * FROM csvread('F:/ELV/data/mortality.csv');
 
 ---- POPULATION
 -- Create:
 CREATE TABLE population(
  year INTEGER NOT NULL,
- settlement INTEGER NOT NULL,
  gender SMALLINT NOT NULL,
  age INTEGER NOT NULL,
+ settlement INTEGER NOT NULL,
  population INTEGER NOT NULL);
 
 -- Index:
 CREATE INDEX population ON population(year, gender, age, settlement);
 
 -- Load:
-insert into population select * from csvread('F:/ELV/data/population.csv');
+INSERT INTO population SELECT * FROM csvread('F:/ELV/data/population.csv');
